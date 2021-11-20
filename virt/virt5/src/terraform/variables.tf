ab42328ad90442c55625a67193a434b1bc680a35
Sergey Chernyshov <sergey_chernyshov_89@mail.ru>
2021-11-20 23:39:28 +0300

    virt hw5

---
+++ virt/virt5/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gjvd13grcv951tkt06"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g91q2tu6if6gi7erv4"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd89tni1qcf7ks80phv5"
+}
